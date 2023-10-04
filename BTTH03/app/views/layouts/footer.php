3d9a5723617a8f8b005e3ef2bb3b77f651347a98
NguyenDucAnh1010 <ducnguyenanh2003@gmail.com>
2023-10-04 09:54:55 +0700

    first commit

--- BTTH03/app/views/layouts/footer.php
+++
@@ -1,7 +0,0 @@
-<div class="card-footer text-center border-top border-3 border-dark mx-5">
-    <span class="fs-5">TLU'S SINH VIEN</span>
-</div>
-<script>
-    var element = document.getElementById('<?php echo $title; ?>');
-    element.className = 'nav-link active';
-</script>
